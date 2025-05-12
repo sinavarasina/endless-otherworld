97043d5a655bf6cbe00c223bdecc1cf9b4e554fc
sinavarasina <varasinafarmadani@gmail.com>
2025-05-13 04:13:19 +0700

    fix bullet scale

--- src/character/bullets/Enemy_Bullets/enemy_bullet_A.py
+++ src/character/bullets/Enemy_Bullets/enemy_bullet_A.py
@@ -7,39 +7,42 @@ from src.logic.collition_logic import CollitionLogic
 from src.components.get_image import SpriteSheet
 from src.character.bullets.bullet_animation import BulletAnimation
 
+
 class EnemyBulletA(Bullet):
     def __init__(self, owner):
         super().__init__(owner)
         self.speed = 15
         self.timeout = 1
 
-        bullet_image_path = os.path.join(ASSET_DIR, "Bullet", "PurpleBulletAnimation.png")
+        bullet_image_path = os.path.join(
+            ASSET_DIR, "Bullet", "PurpleBulletAnimation.png"
+        )
         sprite_sheet = SpriteSheet(pygame.image.load(bullet_image_path).convert_alpha())
 
         self.animation = BulletAnimation(
             sprite_sheet=sprite_sheet,
             frame_width=32,
             frame_height=32,
-            scale=2,
+            scale=1,
             color_key=(0, 0, 0),
             frame_count=6,
-            frame_delay=10
+            frame_delay=10,
         )
 
     def update(self, target_x=None, target_y=None):
         if target_x is None or target_y is None:
             return super().update()
-            
+
         target_x += 24
         target_y += 32
-        
+
         if self.timeout == 150:
             self.reset_bullet()
 
         dx = target_x - self.x
         dy = target_y - self.y
         length = math.hypot(dx, dy)
-        
+
         if self.timeout == 0 and length != 0:
             self.vx = (dx / length) * self.speed
             self.vy = (dy / length) * self.speed
@@ -60,4 +63,3 @@ class EnemyBulletA(Bullet):
         self.vx = 0
         self.vy = 0
         self.active = False
-
