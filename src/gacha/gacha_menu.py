d82c00746bf8a01ba520854b982f02a9c8af69ae
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-05-24 17:06:19 +0700

    changing the spacing to each other items so the word not overlapping

--- src/gacha/gacha_menu.py
+++ src/gacha/gacha_menu.py
@@ -1,3 +1,5 @@
+# i think this ui is good enough, if you wanna change the ui, please tell me first
+
 import pygame
 import os
 import json
@@ -46,16 +48,14 @@ class GachaMenu:
         subtitle2 = font_subtitle.render(
             "Press 1 or 2 or 3 to select the item", True, (200, 200, 200)
         )
-
         title_rect = title.get_rect(center=(self.screen_width // 2, 100))
         subtitle2_rect = subtitle2.get_rect(center=(self.screen_width // 2, self.screen_height - 100))
 
         self.screen.blit(title, title_rect)
         self.screen.blit(subtitle2, subtitle2_rect)
-
         
         box_width, box_height = self.item_size
-        spacing = 100
+        spacing = 300
         total_width = 3 * box_width + 2 * spacing
         start_x = (self.screen_width - total_width) // 2
         y = self.screen_height // 2 - box_height // 2
@@ -65,30 +65,31 @@ class GachaMenu:
             x = start_x + i * (box_width + spacing)
 
             # shaking animation to image
+            # you wanna got a shake huh >w<
             offset_x = random.randint(-2, 2)
             offset_y = random.randint(-2, 2)
 
             border = pygame.Surface((box_width + 10, box_height + 10), pygame.SRCALPHA)
             border.fill((255, 255, 255, 0))
             self.screen.blit(border, (x - 5 + offset_x, y - 5 + offset_y))
-
             self.screen.blit(self.item_images[i], (x + offset_x, y + offset_y))
 
             # number in the top of items image
-            font_number = pygame.font.SysFont(None, 50)
+            font_number = pygame.font.Font(self.font, 50)
             number_text = font_number.render(str(i + 1), True, (255, 255, 255))
             number_rect = number_text.get_rect(center=(x + box_width // 2, y + box_height - 250))
             self.screen.blit(number_text, number_rect)
 
             # description from JSON
-            font_desc = pygame.font.SysFont(None, 30)
+            font_desc = pygame.font.Font(self.font, 30)
             description = self.gacha_data[i]["Description"]
             desc_text = font_desc.render(description, True, (255, 255, 255))
             desc_rect = desc_text.get_rect(center=(x + box_width // 2, y + box_height + 30))
             self.screen.blit(desc_text, desc_rect)
 
         pygame.display.flip()
-
+    
+    # to update this menu in the game loop and checking to key interrupt
     def update(self, game, camera_x, camera_y):
         if game.main_menu:
             self.draw()
