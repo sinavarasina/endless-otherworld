0526b62196f5463b7681db40ef725be9c7d368b0
sinavarasina <varasinafarmadani@gmail.com>
2025-05-25 20:40:16 +0700

    move config file

--- path_config.py
+++ path_config.py
@@ -3,7 +3,5 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ASSET_DIR = os.path.join(BASE_DIR, "assets", "images")
 SOUND_DIR = os.path.join(BASE_DIR, "assets", "sound")
-JSON_DIR = os.path.join(BASE_DIR, "src", "game_data.json")
+JSON_DIR = os.path.join(BASE_DIR, "src", "config", "game_data.json")
 FONT_DIR = os.path.join(BASE_DIR, "assets", "font")
-
-
